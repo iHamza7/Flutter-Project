ae0a7639fb9b1d25f4ba1cd7b3cfc24816b5efcf
Hamzx47 <khanazam3324@gmail.com>
2021-10-06 09:58:32 +0500

    ch1 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "First APP", home: MyApp()));
+  runApp(
+    MaterialApp(
+      title: "First APP",
+      home: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Container(),
+    );
   }
 }
